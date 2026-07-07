b490b9d64b1e3eaa3d1ac309b90dfa0a6f096e28
othmaneslaimani2-jpg <othmaneslaimani2@gmail.com>
2026-07-07 16:11:27 +0100

    add auth middleware to secure the api

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,25 @@
+import jwt from 'jsonwebtoken';
+
+export const authenticate = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).json({ error: 'Access denied. No token provided.'});
+    }
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch (error) {
+        return res.status(401).json({ error: 'Invalid or expired roken.'});
+    }
+};
+export const authorize = (...allowedRoles) => {
+    return (req, res, next) => {
+        if (!req.user || !allowedRoles.includes(req.user.role)) {
+            return res.status(403).json({ error: 'Forbidden. You do not have permission to perform this action. '});
+        }
+        next();
+    };
+};
